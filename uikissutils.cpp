750f55f7e945afbab58f17b8d46de3668d903f89
Mitch Winkle <mitch@radcommsoft.com>
2023-10-08 15:48:43 -0400

    asdf

--- uikissutils.cpp
+++ uikissutils.cpp
@@ -439,7 +439,7 @@ QByteArray UIKISSUtils::buildUIFrame(QString dest_call, QString source_call, QSt
 
 QByteArrayList UIKISSUtils::unwrapUIFrame(QByteArray kiss_in)
 {
-    qDebug()<<"in:"<<kiss_in;
+    qDebug()<<"kiss_in:"<<kiss_in;
     // Erase the KISS mode byte if necessary
     QByteArray in;
     if(kiss_in[0] == 0x00) {
@@ -448,6 +448,7 @@ QByteArrayList UIKISSUtils::unwrapUIFrame(QByteArray kiss_in)
     else {
         in = kiss_in;
     }
+    qDebug()<<"in:"<<in;
     //                                     opt   opt
     //                    dest,src,digi1,digi2,payload
     QByteArrayList out = {"", "", "", "", ""};
